23ffe00381add7d2a4617c653ae0833dccf888f6
rceballos05 <73326252+rceballos05@users.noreply.github.com>
2025-10-10 13:42:34 -0300

    Refactor perfil page into pages module

---
+++ lib/models/usuario.dart
@@ -0,0 +1,11 @@
+class Usuario {
+  const Usuario({
+    required this.id,
+    required this.nombre,
+    required this.correo,
+  });
+
+  final String id;
+  final String nombre;
+  final String correo;
+}
