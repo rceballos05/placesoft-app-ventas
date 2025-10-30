1e7244bf91d640d63b4fd3e924a554286e50ca5b
rceballos05 <73326252+rceballos05@users.noreply.github.com>
2025-10-30 10:46:09 -0300

    Implement SQLite client search for BuscarCliente

--- lib/models/cliente.dart
+++ lib/models/cliente.dart
@@ -1,13 +1,22 @@
 class Cliente {
   const Cliente({
-    required this.codigo,
+    required this.rut,
     required this.nombre,
     this.direccion,
     this.comuna,
   });
 
-  final String codigo;
+  final String rut;
   final String nombre;
   final String? direccion;
   final String? comuna;
+
+  factory Cliente.fromMap(Map<String, dynamic> map) {
+    return Cliente(
+      rut: (map['rut'] ?? '').toString(),
+      nombre: (map['nombre'] ?? '').toString(),
+      direccion: map['direccion'] as String?,
+      comuna: map['comuna'] as String?,
+    );
+  }
 }
